fd2ba2c821146b968643d385456ccf1dc66eb296
temur <@timur>
2024-04-10 12:18:36 +0500

    Rkp Temur

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import '~/assets/scss/style.scss'
 
 import './styles/main.scss'
 import DatePicker from './modules/datepicker'
@@ -9,51 +8,82 @@ import Pinia from './modules/pinia'
 import PWA from './modules/pwa'
 import VeeValidate from './modules/vee-validate'
 import router from '~/router'
-import { $individual } from '~/services/baseHttp'
 import { useOrganizationStore } from '~/stores/organization'
 import { useToken } from '~/composables/useToken'
 import { IS_DEV } from '~/utils/config'
 
-const { removeToken, getToken, handleLogout } = useToken()
-
 const app = createApp(App)
+
+app.use(VeeValidate)
+app.use(Pinia)
+app.use(I18n)
+app.use(router)
+app.use(PWA)
+app.use(DatePicker)
+
+const { getOrganization } = useOrganizationStore()
+const { removeToken, getToken } = useToken()
+
 const token = getToken()
-if (!token) { handleLogout() }
-else {
-  $individual.interceptors.response.use(
-    (response) => {
-      return response
-    },
-    (error) => {
-      if (error.response.status === 401) {
-        removeToken()
-        handleLogout()
-      }
 
-      return Promise.reject(error)
-    },
-  )
+function checkToken() {
+  // if (!token && !IS_DEV) window.location.href = VITE_IDENTITY_SERVICE;
+}
 
-  app.use(DatePicker).use(Pinia).use(PWA).use(VeeValidate).use(I18n)
-  const { getOrganization } = useOrganizationStore()
+checkToken();
 
-  if (token) {
-    getOrganization()
-      .catch(() => {
-        if (!IS_DEV) {
-          setTimeout(() => {
-            handleLogout()
-            removeToken()
-          })
-        }
-      })
-      .finally(() => {
-        app.use(router)
-        app.mount('#app')
-      })
+(async function fetchDate() {
+  try {
+    await getOrganization()
   }
-  else {
-    app.use(router)
+  catch (err: any) {
+    if (!IS_DEV)
+      removeToken()
+      // window.location.href = VITE_IDENTITY_SERVICE;
+  }
+  finally {
     app.mount('#app')
   }
-}
+})()
+
+// const app = createApp(App)
+// const token = getToken()
+// if (!token) { handleLogout() }
+// else {
+//   $http.interceptors.response.use(
+//     (response) => {
+//       return response
+//     },
+//     (error) => {
+//       if (error.response.status === 401) {
+//         removeToken()
+//         handleLogout()
+//       }
+//
+//       return Promise.reject(error)
+//     },
+//   )
+//
+//   app.use(DatePicker).use(Pinia).use(PWA).use(VeeValidate).use(I18n)
+//   const { getOrganization } = useOrganizationStore()
+//
+//   if (token) {
+//     getOrganization()
+//       .catch(() => {
+//         if (!IS_DEV) {
+//           setTimeout(() => {
+//             handleLogout()
+//             removeToken()
+//           })
+//         }
+//       })
+//       .finally(() => {
+//         app.use(router)
+//         app.mount('#app')
+//       })
+//   }
+//   else {
+//     app.use(router)
+//     app.mount('#app')
+//   }
+// }
